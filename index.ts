2de5b84964bc5627d15d6e9a124d5854af809d16
Michael <msquaremega@gmail.com>
2023-05-10 23:20:14 +0100

    work with mongo

--- index.ts
+++ index.ts
@@ -1,14 +1,29 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import helmet from 'helmet';
+import rate_limit from 'express-rate-limit';
+
 
 const port = process.env.HTTP_PORT || 3001;
 
 const app = express();
 
+// Rate limiting
+app.set('trust proxy', 1);
+app.use(
+    rate_limit({
+        windowMs: 15 * 60 * 1000,
+        max: 1000,
+        message: "You can't make any more request at the moment, try again later"
+    })
+);
+
 app.use(express.json());
 
-app.use(express.urlencoded({ extended: true}))
+// app.use(express.urlencoded({ extended: true}));
+
+app.use(helmet());
 
 app.get('/', (req, res) => {
     res.send('Yesssssssss Hello')
