5f277c4f69178d398e3f7b71ab29bd5bc1b18f64
Michael <msquaremega@gmail.com>
2023-05-15 21:56:48 +0100

    save user with encrypted password done

--- models/users.ts
+++ models/users.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
-import validator from "validator"
+import validator from "validator";
+import bcrypt from "bcrypt";
 
 const UserSchema = new mongoose.Schema({
     firstName: {
@@ -33,5 +34,6 @@ const UserSchema = new mongoose.Schema({
 });
 
 UserSchema.pre('save', async function () {
-    
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt)
 });
