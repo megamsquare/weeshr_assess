69c3c840b98311984f0792ffd9660f2e732f6dee
Michael <msquaremega@gmail.com>
2023-05-24 17:39:56 +0100

    create auth middleware and not_found middleware and implement not found middleware

---
+++ src/middleware/not_found.middleware.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+import status_code from 'http-status';
+
+function not_found(req: Request, res: Response) {
+    res.status(status_code.NOT_FOUND).json({message: 'Route does not exist'});
+    return;
+}
+
+export default not_found;
