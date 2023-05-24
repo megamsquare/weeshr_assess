02334d36b6229b3004e45ba11a6e3f77b8747e6f
Michael <msquaremega@gmail.com>
2023-05-24 17:28:42 +0100

    changes to user router

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,6 +1,9 @@
 import express from 'express';
+import user_controller from '../controllers/user.controller'
 
 const routers =  express.Router();
+
+routers.post('/', );
 
 // Export the rtouter
 export default routers;
