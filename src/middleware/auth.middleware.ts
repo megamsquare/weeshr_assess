581e183f5ca7e6dc0f9231ef586350fb3b656d7a
Michael <msquaremega@gmail.com>
2023-05-25 06:07:21 +0100

    check bearer

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,4 +1,14 @@
 import { Request, Response, NextFunction } from "express";
-function verify_token(req: Request, res: Response, next: NextFunction) {}
+import status_code from "http-status"
+import Err from "../use_cases/error_handler";
+
+function verify_token(req: Request, res: Response, next: NextFunction) {
+    let token;
+    const header = req.headers.authorization;
+    if (!header || !header.startsWith('Bearer')) {
+        res.status(status_code.UNAUTHORIZED).json({ message: Err.Unauthentication });
+        return;
+    }
+}
 
 function verify_permission(roles: string[]) {}
