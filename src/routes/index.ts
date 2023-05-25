9af81c68047e7d76e19fd70a6468b99cb888a295
Michael <msquaremega@gmail.com>
2023-05-25 06:56:55 +0100

    make the api version 1

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,7 @@ import user_route from './user.route';
 const routers = express.Router();
 
 // Mount the individual routes here
-routers.use('/user', user_route);
+routers.use('/v1/user', user_route);
 
 // Export the routes
 export default routers;
