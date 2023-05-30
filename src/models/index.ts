3dd07418cac908e16e2267ea3f1d4e4e37984941
Michael <msquaremega@gmail.com>
2023-05-30 14:39:03 +0100

    import tokens to index model

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,9 +1,11 @@
 import User from "./users.model";
 import Roles from "./roles.model";
+import Tokens from "./token.model";
 
 const Model = {
     User,
-    Roles
+    Roles,
+    Tokens
 }
 
 export default Model;
