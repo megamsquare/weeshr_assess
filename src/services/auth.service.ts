031f641dd86c76fb6082fbeafc0b42419262795d
Michael <msquaremega@gmail.com>
2023-06-12 22:43:11 +0100

    add neccessary imports for auth service

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,3 +1,9 @@
+import Model from "../models";
+import DB from "../db";
+import Err from "../use_cases/error_handler";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
+
 async function login() {
     
 }
