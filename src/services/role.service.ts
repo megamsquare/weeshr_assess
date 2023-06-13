147a28a8d1229407029bac31cfe3e31156749194
Michael <msquaremega@gmail.com>
2023-06-13 14:48:28 +0100

    create role service which most used functions

--- src/services/role.service.ts
+++ src/services/role.service.ts
@@ -1,2 +1,20 @@
 import Model from "../models";
 import Err from "../use_cases/error_handler";
+
+async function createRole() {
+    
+}
+
+async function getRoleById() {
+    
+}
+
+async function getRoleByUserId() {
+    
+}
+
+const RoleService = {
+    createRole,
+    getRoleById,
+    getRoleByUserId
+}
