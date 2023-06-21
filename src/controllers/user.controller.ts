b2965da4602dbd5fac27058ce06334efeca8e52e
Michael <msquaremega@gmail.com>
2023-06-21 19:15:52 +0100

    clean code

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -5,15 +5,11 @@ import { NewUser } from "../use_cases/obj/user.case";
 import Services from "../services";
 
 async function createUser(req: Request, res: Response) {
-       try {
-
-        const user_model = Model.User;
-        const role_model = Model.Roles;
-
+    try {
         let userInfo: NewUser;
 
         userInfo = req.body;
-    
+
         const user = await Services.UserService.createUser(userInfo);
         if (user instanceof Error) {
             res.status(status_code.BAD_REQUEST).json({ message: user.message });
@@ -27,31 +23,31 @@ async function createUser(req: Request, res: Response) {
             email: user.email
         }
 
-        res.status(status_code.CREATED).json({ data: {user: userDetails}})
+        res.status(status_code.CREATED).json({ data: { user: userDetails } })
 
-       } catch (error) {
+    } catch (error) {
         res.status(status_code.BAD_REQUEST).json({ message: error })
-       }
+    }
 }
 
-async function updateUser(req:Request, res: Response) {
+async function updateUser(req: Request, res: Response) {
 
 }
 
-async function getUserById(req:Request, res: Response) {
-    
+async function getUserById(req: Request, res: Response) {
+
 }
 
-async function getAllUser(req:Request, res: Response) {
-    
+async function getAllUser(req: Request, res: Response) {
+
 }
 
-async function getUserByUsername(req:Request, res: Response) {
-    
+async function getUserByUsername(req: Request, res: Response) {
+
 }
 
-async function getUserByEmail(req:Request, res: Response) {
-    
+async function getUserByEmail(req: Request, res: Response) {
+
 }
 
 const User_controller = {
