5eb16f510c3514e4c4718dd380bba3a799aedafe
Michael <msquaremega@gmail.com>
2023-06-21 19:15:12 +0100

    creating user in user controller done

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,39 +1,30 @@
 import { Request, Response } from "express";
 import status_code from "http-status";
 import Model from "../models";
+import { NewUser } from "../use_cases/obj/user.case";
+import Services from "../services";
 
 async function createUser(req: Request, res: Response) {
        try {
 
         const user_model = Model.User;
         const role_model = Model.Roles;
-        
-        const email_exist = await user_model.exists({ email: req.body.email })
-        if (email_exist) {
-            res.status(status_code.BAD_REQUEST).json({ message: 'Email is already taken' })
-            return
-        }
 
-        const username_exist = await user_model.exists({ username: req.body.username })
-        if (username_exist) {
-            res.status(status_code.BAD_REQUEST).json({ message: 'Username is already taken' })
-            return
-        }
+        let userInfo: NewUser;
+
+        userInfo = req.body;
     
-        const user = await user_model.create({ ...req.body })
-
-        const save_role = {
-            userId: user._id,
-            role: "user"
+        const user = await Services.UserService.createUser(userInfo);
+        if (user instanceof Error) {
+            res.status(status_code.BAD_REQUEST).json({ message: user.message });
+            return;
         }
-
-        const role = await role_model.create({ ...save_role })
 
         const userDetails = {
+            userId: user._id,
             name: `${user.firstName} ${user.lastName}`,
             username: user.username,
-            email: user.email,
-            role: role.role
+            email: user.email
         }
 
         res.status(status_code.CREATED).json({ data: {user: userDetails}})
