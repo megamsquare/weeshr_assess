f2eadcb232cd64add3ed7f4482b7e58fb422c4de
Michael <msquaremega@gmail.com>
2023-07-05 16:37:03 +0100

    create a email transporter function that will create connection to the email sender

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -1,7 +1,22 @@
-import NodeMailer from 'nodemailer'
+import nodeMailer from 'nodemailer'
 
-async function emailTransport(email:string) {
-    
+async function emailTransport() {
+    const email = process.env.EMAIL || '';
+    const pass = process.env.PASS || '';
+    try {
+        const config = {
+            service: 'gmail',
+            auth: {
+                user: email,
+                pass: pass
+            }
+        }
+        const transporter = nodeMailer.createTransport(config);
+
+        return transporter
+    } catch (error) {
+        return error as Error;
+    }
 }
 
 async function sendEmail(email:string) {
