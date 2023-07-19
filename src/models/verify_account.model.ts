7479546eb87c983ce6fa14e99f8c06112f95169f
Michael <msquaremega@gmail.com>
2023-07-19 16:39:37 +0100

    create verify account schema

---
+++ src/models/verify_account.model.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const VerifyAccountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'users',
+        required: true
+    },
+    verificationCode: {
+        type: Number,
+        required: true
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    },
+    deletedAt: {
+        type: Date,
+    }
+},
+{
+    timestamps: true
+})
